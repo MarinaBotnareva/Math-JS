215048bd93c7927a3fd9804e9af6467c14a04b87
MarinaBotnareva <botmarinka@gmail.com>
2022-01-25 17:25:56 +0200

    Initual files

---
+++ index.js
@@ -0,0 +1,34 @@
+/* Площадь треугольника
+let a = Number(prompt('Сторона треугольника'));
+    ha = Number(prompt('Высота к этой стороне'));
+let s = 1/2*a*ha;
+console.log(s);*/
+
+/*Площадь трапеции
+let a = Number(prompt('Верхнее основание трапеции'));
+    b = Number(prompt('Нижнее основание трапеции'));
+    h = Number(prompt('Высота трапеции'));
+    s = 1/2*(a+b)*h;
+console.log(s);*/
+
+/*Площадь круга
+let r = Number(prompt('Радиус круга'));
+    s = Math.PI*Math.pow(r, 2);
+console.log(s); */
+
+/* Площадь параллелограмма
+let a = Number(prompt('Сторона параллелограмма'));
+    ha = Number(prompt('Высота к этой стороне'));
+let s = a*ha;
+console.log(s);*/
+
+/* Площадь цилиндра
+let r = Number(prompt('Радиус основания'));
+    h = Number(prompt('Высота цилиндра'));
+    s = Math.PI*Math.pow(r, 2)*h;
+console.log(s);*/
+
+/*Площадь сферы */
+let r = Number(prompt('Радиус сферы'));
+    s = 4*Math.PI*Math.pow(r, 2);
+console.log(s); 
